50b0ad574126c461b6f07e21bfd2e1483165fb81
fterlid <fterlid@gmail.com>
2015-04-02 04:01:20 +0200

    Added some white space.

--- src/Fte.Ioc/Fte.Ioc/Facade/IContainer.cs
+++ src/Fte.Ioc/Fte.Ioc/Facade/IContainer.cs
@@ -5,8 +5,11 @@ namespace Fte.Ioc.Facade
 	public interface IContainer
 	{
 		void Register<TAbstraction, TConcrete>() where TConcrete : TAbstraction;
+
 		void Register<TAbstraction, TConcrete>(LifeCycle lifeCycle) where TConcrete : TAbstraction;
+
 		T Resolve<T>(Type typeToResolve) where T : class;
+
 		object Resolve(Type typeToResolve);
     }
 }
