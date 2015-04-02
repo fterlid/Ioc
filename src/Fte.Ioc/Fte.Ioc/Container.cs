441d03d47d559cfdf95cb2812bb6232ffff93a86
fterlid <fterlid@gmail.com>
2015-04-02 02:52:56 +0200

    Added method to facade. Added missing access modifier.

---
+++ src/Fte.Ioc/Fte.Ioc/Container.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fte.Ioc
+{
+	public class Container
+	{
+		public void Register<TAbstraction, TConcrete>() where TConcrete : TAbstraction
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Register<TAbstraction, TConcrete>(LifeCycle lifeCycle) where TConcrete : TAbstraction
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Resolve<T>(Type typeToResolve) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Resolve(Type typeToResolve)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
