21e49918827c574740dbe6293ff415588711b5d0
fterlid <fterlid@gmail.com>
2015-04-26 21:31:21 +0200

    Added test for ContainerProvider.

---
+++ src/Fte.Ioc/Fte.Ioc.Tests/ContainerProviderTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fte.Ioc.Tests
+{
+	[TestClass]
+	public class ContainerProviderTest
+	{
+		[TestMethod]
+		public void GetContainer_MethodIsCalled_ReturnsNonNullObject()
+		{
+			var container = ContainerProvider.GetContainer();
+			Assert.IsNotNull(container);
+		}
+	}
+}
