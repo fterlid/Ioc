6016be068ef4fc53bbb8412098520761fea3dd08
fterlid <fterlid@gmail.com>
2015-05-01 23:54:03 +0200

    Refactored TypeResolverTest to use NUnit.

--- src/Fte.Ioc/Fte.Ioc.Tests/Resolver/TypeResolverTest.cs
+++ src/Fte.Ioc/Fte.Ioc.Tests/Resolver/TypeResolverTest.cs
@@ -1,53 +1,65 @@
-﻿using Fte.Ioc.Exceptions;
+﻿using System;
+using Fte.Ioc.Exceptions;
+using Fte.Ioc.ObjectManagement;
 using Fte.Ioc.Registry;
 using Fte.Ioc.Resolver;
 using Fte.Ioc.Tests.Utils.TestServices;
 using Fte.Ioc.Tests.Utils.TestServices.CircularDependencies;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
-using Fte.Ioc.ObjectManagement;
+using NUnit.Framework;
 
 namespace Fte.Ioc.Tests.Resolver
 {
-	[TestClass]
+	[TestFixture]
 	public class TypeResolverTest
 	{
 		private Mock<ITypeRegistry> _registryMock;
 		private Mock<IObjectManager> _objectManagerMock;
 		private ITypeResolver _resolver;
 
-		[TestInitialize]
-		public void TestInitialize()
+		[SetUp]
+		public void Setup()
 		{
 			_registryMock = new Mock<ITypeRegistry>();
 			_objectManagerMock = new Mock<IObjectManager>();
 			_resolver = new TypeResolver(_registryMock.Object, _objectManagerMock.Object);
 		}
 
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
+		[Test]
 		public void Ctor_TypeRegistryAsNull_ThrowsException()
 		{
-			new TypeResolver(null, _objectManagerMock.Object);
+			var ex = Assert.Catch<ArgumentNullException>(() =>
+			{
+				new TypeResolver(null, _objectManagerMock.Object);
+			});
+
+			StringAssert.Contains("typeRegistry", ex.Message);
 		}
 
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
-		public void Ctor_ObjectFactoryAsNull_ThrowsException()
+		[Test]
+		public void Ctor_ObjectManagerAsNull_ThrowsException()
 		{
-			new TypeResolver(_registryMock.Object, null);
+			var ex = Assert.Catch<ArgumentNullException>(() =>
+			{
+				new TypeResolver(_registryMock.Object, null);
+			});
+
+			StringAssert.Contains("objectManager", ex.Message);
 		}
 
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
+		[Test]
 		public void Resolve_TypeIsNull_ThrowsException()
 		{
-			_resolver.Resolve(null);
+			var ex = Assert.Catch<ArgumentNullException>(() =>
+			{
+				_resolver.Resolve(null);
+			});
+
+			StringAssert.Contains("typeToResolve", ex.Message);
 		}
 
-		[TestMethod]
-		public void Resolve_TypeIsRegisteredAndHasDefaultConstructor_ObjectIsResolved()
+		[Test]
+		public void Resolve_TypeWithDefaultConstructorIsRegistered_ObjectIsResolved()
 		{
 			RegisterType(typeof(ITestService), typeof(TestService), LifeCycle.Singleton);
 
@@ -56,8 +68,8 @@ namespace Fte.Ioc.Tests.Resolver
 			_objectManagerMock.Verify(x => x.Create(It.IsAny<TypeRegistryItem>(), It.IsAny<object[]>()), Times.Once);
 		}
 
-		[TestMethod]
-		public void Resolve_TypeIsRegisteredAndHasRegisteredDependency_ObjectIsResolved()
+		[Test]
+		public void Resolve_TypeWithRegisteredDependencyIsRegistered_ObjectIsResolved()
 		{
 			RegisterType(typeof(ITestService), typeof(TestService), LifeCycle.Singleton);
 			RegisterType(typeof(IOtherTestService), typeof(OtherTestService), LifeCycle.Singleton);
@@ -67,7 +79,7 @@ namespace Fte.Ioc.Tests.Resolver
 			_objectManagerMock.Verify(x => x.Create(It.IsAny<TypeRegistryItem>(), It.IsAny<object[]>()), Times.Exactly(2));
 		}
 
-		[TestMethod]
+		[Test]
 		public void Resolve_SingeltonWithTransientDependency_NoObjectsAreCreatedIfSingletonHasBeenCreated()
 		{
 			RegisterType(typeof(IOtherTestService), typeof(OtherTestService), LifeCycle.Singleton);
@@ -78,7 +90,7 @@ namespace Fte.Ioc.Tests.Resolver
 			_objectManagerMock.Verify(x => x.Create(It.IsAny<TypeRegistryItem>(), It.IsAny<object[]>()), Times.Never);
 		}
 
-		[TestMethod]
+		[Test]
 		public void Resolve_TransientWithSingletonDependency_SingletonIsCreatedOnce()
 		{
 			RegisterType(typeof(ITestService), typeof(TestService), LifeCycle.Singleton);
@@ -92,30 +104,30 @@ namespace Fte.Ioc.Tests.Resolver
 			_objectManagerMock.Verify(x => x.Create(It.IsAny<TypeRegistryItem>(), It.IsAny<object[]>()), Times.Once);
 		}
 
-		[TestMethod]
-		[Timeout(5000)] //Timeout if circular depenceny is not handled
-		[ExpectedException(typeof(CircularDependencyException))]
+		[Test, Timeout(3000)] //Timeout if circular depenceny is not handled
 		public void Resolve_TypeHasDegenerateCircularDependency_ThrowsException()
 		{
 			RegisterType(typeof(IDegenerateCircularDependency), typeof(DegenerateCircularDependency), LifeCycle.Transient);
 
-			_resolver.Resolve(typeof(IDegenerateCircularDependency));
+			Assert.Catch<CircularDependencyException>(() =>
+			{
+				_resolver.Resolve(typeof(IDegenerateCircularDependency));
+			});
 		}
 
-		[TestMethod]
-		[Timeout(5000)]	//Timeout if circular depenceny is not handled
-		[ExpectedException(typeof(CircularDependencyException))]
+		[Test, Timeout(3000)] //Timeout if circular depenceny is not handled
 		public void Resolve_TypeHasSimpleCircularDependency_ThrowsException()
 		{
 			RegisterType(typeof(ISimpleCircularDepenceny), typeof(SimpleCircularDepenceny), LifeCycle.Transient);
 			RegisterType(typeof(IOtherSimpleCircularDepenceny), typeof(OtherSimpleCircularDepenceny), LifeCycle.Transient);
 
-			_resolver.Resolve(typeof(ISimpleCircularDepenceny));
+			Assert.Catch<CircularDependencyException>(() =>
+			{
+				_resolver.Resolve(typeof(ISimpleCircularDepenceny));
+			});
 		}
 
-		[TestMethod]
-		[Timeout(5000)]	//Timeout if circular depenceny is not handled
-		[ExpectedException(typeof(CircularDependencyException))]
+		[Test, Timeout(3000)] //Timeout if circular depenceny is not handled
 		public void Resolve_TypeHasComplexCircularDependency_ThrowsException()
 		{
 			RegisterType(typeof(IComplexCircularDependency), typeof(ComplexCircularDependency), LifeCycle.Transient);
@@ -125,7 +137,10 @@ namespace Fte.Ioc.Tests.Resolver
 			RegisterType(typeof(IComplexCircularDependencyD), typeof(ComplexCircularDependencyD), LifeCycle.Transient);
 			RegisterType(typeof(IComplexCircularDependencyE), typeof(ComplexCircularDependencyE), LifeCycle.Transient);
 
-			_resolver.Resolve(typeof(IComplexCircularDependency));
+			Assert.Catch<CircularDependencyException>(() =>
+			{
+				_resolver.Resolve(typeof(IComplexCircularDependency));
+			});
 		}
 
 		private void RegisterType(Type abstraction, Type concrete, LifeCycle lifeCycle)
